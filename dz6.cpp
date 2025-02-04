9f9fb6de4a0444215889968fe194494f2f3baa2c
Бордюжа Марк <borduzamark@gmail.com>
2025-02-04 23:43:48 +0200

    Add files via upload

---
+++ dz6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+    int sum = 0;
+    int number;
+
+
+    int i = 0;
+    while (i < 10)
+    {
+        if (i % 2 != 0)
+            continue;
+
+        std::cout << i << " ";
+        i++;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (i % 2 != 0)
+            continue;
+
+        std::cout << i << " ";  //вічний цикл
+    } 
+
+    return 0;
+}
